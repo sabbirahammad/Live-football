48c3ff07848b26dba450a56658f47a06c34653e0
Sabbir Ahammad < @example.com>
2026-06-08 22:43:53 +0600

    Your descriptive commit message here

--- routes/appConfigRoutes.js
+++ routes/appConfigRoutes.js
@@ -5,6 +5,6 @@ import { protect, admin } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.get('/update', getUpdateInfo);
-router.put('/update', protect, admin, updateAppConfig);
+router.put('/update', admin, updateAppConfig);
 
 export default router;
